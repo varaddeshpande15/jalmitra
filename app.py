2d14a30d0753b2c631c1e213ab620107874ddec8
Kabeer Ahmed Merchant <59280736+Kabeer2004@users.noreply.github.com>
2023-11-13 18:30:34 +0530

    the v0.3 commit
    added login and session handling logic
    
    added access restrictions - baselevel users can no longer access admin features - more access restrictions have to still be put in place but this system works for now
    
    added dashboards for users and admins - these serve as main menus for admins and users to access various features of the apps on their end. 
    
    added some basic CSS styling for frontend.

--- app.py
+++ app.py
@@ -31,8 +31,8 @@ def allowed_file(filename):
 def index():
     return render_template('index.html')
 
-@app.route('/admin_dashboard')
-def admin_dashboard():
+@app.route('/admin_table')
+def admin_table():
     # Generate data for the main table
     table_data = []
     for index, item in enumerate(main_table):
@@ -50,7 +50,7 @@ def admin_dashboard():
         })
 
     # Pass the data to the template
-    return render_template('admin_dashboard.html', table_data=table_data)
+    return render_template('admin_table.html', table_data=table_data)
 
 @app.route('/admin_login', methods=['GET', 'POST'])
 def admin_login():
@@ -64,7 +64,9 @@ def admin_login():
                 # Check if the entered password matches the stored password
                 if admin[username] == password:
                     # Redirect to the admin_dashboard page
-                    return redirect(url_for('admin_dashboard'))
+                    session['username'] = username
+                    session['password'] = password
+                    return render_template('admin_dashboard.html')
 
     # If username or password is incorrect, refresh the page
     return render_template('admin_login.html')
@@ -81,6 +83,7 @@ def user_login():
             if username in user and user[username] == password:
                 # Store the username in the session to track the logged-in user
                 session['username'] = username
+                session['password'] = password
                 return redirect(url_for('user_dashboard'))
 
     return render_template('user_login.html')
@@ -89,13 +92,53 @@ def user_login():
 def user_dashboard():
     # Retrieve the currently logged-in user from the session
     username = session.get('username', None)
-
+    password = session.get('password', None)
     # Redirect to user_login if no user is logged in
     if not username:
         return redirect(url_for('user_login'))
 
     # Render the user_dashboard.html page with the logged-in user
-    return render_template('user_dashboard.html', username=username)
+    return render_template('user_dashboard.html', username=username, password=password)
+
+@app.route('/forbidden')
+def forbidden():
+    return render_template('forbidden.html')
+
+@app.route('/admin_dashboard')
+def admin_dashboard():
+    # Check if the user is logged in
+    if 'username' not in session:
+        return redirect(url_for('index'))
+
+    # Check if the logged-in user is an admin
+    username = session['username']
+    password = session.get('password')
+    
+    is_admin = any(username in admin and admin[username] == password for admin in admin_list)
+
+    # Check if the user is an admin
+    if not is_admin:
+        return(redirect(url_for('forbidden')))  # Forbidden
+
+    # Fetch data for the admin dashboard
+    table_data = []
+    for index, item in enumerate(main_table):
+        # Generate URLs for the images
+        image_url = url_for('uploaded_file', filename=item['image'])
+        
+        # Add more data for each row in the table
+        table_data.append({
+            'id': index + 1,
+            'image': f'<img src="{image_url}" alt="Image" style="max-width: 100px; max-height: 100px;">',
+            'problem_title': item['problem_title'],
+            'severity': item['severity'],
+            'location': item['location'],
+            'reporter': item['reporter']
+        })
+
+
+    # Render the admin dashboard
+    return render_template('admin_dashboard.html', table_data=table_data, username=username)
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
@@ -146,6 +189,14 @@ def user_signup():
 
     return render_template('user_signup.html', message=None)
 
+@app.route('/logout')
+def logout():
+    # Clear any session variables related to user login
+    session.pop('username', None)  # Assuming 'username' is the session key for the logged-in user
+    # You may need to clear other session variables as well
+
+    # Redirect to the homepage or login page
+    return redirect(url_for('index'))  # Change 'index' to the actual route of your homepage or login page
 
 # Add a route to serve uploaded images
 @app.route('/uploads/<filename>')
